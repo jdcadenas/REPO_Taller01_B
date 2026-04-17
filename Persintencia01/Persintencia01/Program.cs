f65449f34e2b3628b0a46718a2298303ca19b24b
jdcadenas <jdcadenas@gmail.com>
2026-04-17 14:52:46 -0400

    agregamos el directiorio

--- Persintencia01/Persintencia01/Program.cs
+++ Persintencia01/Persintencia01/Program.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.IO;
 
 namespace Persintencia01
 {
@@ -17,7 +18,24 @@ namespace Persintencia01
 			Console.WriteLine("==== TALLER SECCION B =====");
 			
 			
-			// TODO: Implement Functionality Here
+			// directorio
+			
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DATOSIUJO");
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTES");
+			
+			Console.WriteLine(rutaRaiz);
+			Console.WriteLine(rutaReportes);
+			
+			if(!Directory.Exists(rutaReportes)){
+			   //crear el directorio reportes
+			   	Directory.CreateDirectory(rutaReportes);
+			   	Console.WriteLine("Directorio creado correctamente");
+			   
+			   }
+			
+			
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
